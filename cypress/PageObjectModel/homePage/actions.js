f72263124bc620b75d678a6b7c9d9628b9896af9
waleedafifi90 <waleedafifi.blog@gmail.com>
2021-12-14 11:59:48 +0200

    add 4 action 2 for hover effect and two for clicking on the category then click on the car year

--- cypress/PageObjectModel/homePage/actions.js
+++ cypress/PageObjectModel/homePage/actions.js
@@ -5,8 +5,25 @@ export class HomePageActions {
     this.items = new HomeItems();
   }
 
+  hoverActionOnCarType(item) {
+    this.items.vehicleContainerCarType(item)
+        .realHover()
+        .wait(1000);
+  }
+
   selectCarType(item) {
     this.items.vehicleContainerCarType(item)
+        .click();
+  }
+
+  hoverActionOnCarItem(item) {
+    this.items.carContainerItemsByType(item)
+        .realHover()
+        .wait(1000);
+  }
+
+  selectCarItemFromCategory(item) {
+    this.items.carContainerItemsByType(item)
         .click();
   }
 }
